e9d1c980a944257546b96c6c182dd97f04d0f1d3
pouya <p.ebrahimi163@gmail.com>
2025-03-07 00:08:00 +0330

    feat: render all steps

--- src/pages/createUser/CreateUserForm.tsx
+++ src/pages/createUser/CreateUserForm.tsx
@@ -1,16 +1,23 @@
 import PersonalInfoStep from "./ui/PersonalInfoStep.tsx";
+import {IUser} from "../../entities/user.ts";
+import JobInfoStep from "./ui/JobInfoStep.tsx";
+import ContactInfoStep from "./ui/ContactInfoStep.tsx";
+
+const user: IUser = {
+    name: 'ali',
+    family: 'ali',
+    email: 'a.ali@gmail.com',
+    phone_number: '09121111111',
+    job: '',
+    job_description: ''
+}
 
 const CreateUserForm = () => {
     return (
         <div>
-            <PersonalInfoStep user={{
-                name: 'ali',
-                family: 'ali',
-                email: 'a.ali@gmail.com',
-                phone_number: '09121111111',
-                job: '',
-                job_description: ''
-            }}/>
+            <PersonalInfoStep user={user}/>
+            <ContactInfoStep user={user}/>
+            <JobInfoStep user={user}/>
         </div>
     );
 };
